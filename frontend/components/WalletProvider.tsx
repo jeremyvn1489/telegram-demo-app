0354752b430809d6711dcb820586b6bbe9d9ff82
jeremyvn1489 <jeremyvn1489@gmail.com>
2024-12-02 04:10:44 +0700

    fix wallet connect

--- frontend/components/WalletProvider.tsx
+++ frontend/components/WalletProvider.tsx
@@ -1,5 +1,12 @@
 import { PropsWithChildren } from "react";
 import { AptosWalletAdapterProvider } from "@aptos-labs/wallet-adapter-react";
+import { BitgetWallet } from "@bitget-wallet/aptos-wallet-adapter";
+import { MartianWallet } from "@martianwallet/aptos-wallet-adapter";
+import { MSafeWalletAdapter } from "@msafe/aptos-wallet-adapter";
+import { OKXWallet } from "@okwallet/aptos-wallet-adapter";
+import { PontemWallet } from "@pontem/wallet-adapter-plugin";
+import { TrustWallet } from "@trustwallet/aptos-wallet-adapter";
+import { FewchaWallet } from "fewcha-plugin-wallet-adapter";
 // Internal components
 import { useToast } from "@/components/ui/use-toast";
 // Internal constants
@@ -8,20 +15,29 @@ import { NETWORK, APTOS_API_KEY } from "@/constants";
 export function WalletProvider({ children }: PropsWithChildren) {
   const { toast } = useToast();
 
+  const wallets = [
+    new BitgetWallet(),
+    new FewchaWallet(),
+    new MartianWallet(),
+    new MSafeWalletAdapter(),
+    new PontemWallet(),
+    new TrustWallet(),
+    new OKXWallet(),
+  ];
+
   return (
     <AptosWalletAdapterProvider
+      plugins={wallets}
       autoConnect={true}
       dappConfig={{
         network: NETWORK,
+        aptosApiKey: APTOS_API_KEY,
+        // aptosConnectDappId: "",
         mizuwallet: {
-          // Learn more https://docs.mizu.io/docs/preparation/mizu-app-id
-          appId: undefined,
-          // Learn more https://docs.mizu.io/docs/preparation/manifest-json
           manifestURL: "https://assets.mz.xyz/static/config/mizuwallet-connect-manifest.json",
-          aptosApiKey: APTOS_API_KEY,
         },
       }}
-      optInWallets={["Mizu Wallet"]}
+      // optInWallets={["Mizu Wallet"]}
       onError={(error) => {
         toast({
           variant: "destructive",
--- frontend/components/WalletSelector.tsx
+++ frontend/components/WalletSelector.tsx
@@ -1,6 +1,35 @@
-import { WalletItem, isInstallRequired, truncateAddress, useWallet } from "@aptos-labs/wallet-adapter-react";
-import { Copy, LogOut } from "lucide-react";
-import { useCallback } from "react";
+// @ts-ignore
+import {
+  WalletItem,
+  // @ts-ignore
+  isInstallRequired,
+  truncateAddress,
+  useWallet,
+  WalletSortingOptions, groupAndSortWallets, AboutAptosConnect,
+  // @ts-ignore
+  APTOS_CONNECT_ACCOUNT_URL,
+  AboutAptosConnectEducationScreen,
+  // @ts-ignore
+  AnyAptosWallet,
+  AptosPrivacyPolicy,
+  // @ts-ignore
+  isAptosConnectWallet,
+} from "@aptos-labs/wallet-adapter-react";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "./ui/collapsible";
+import {
+  ArrowLeft,
+  ArrowRight,
+  ChevronDown,
+  Copy,
+  LogOut,
+  // @ts-ignore
+  User,
+} from "lucide-react";
+import { useCallback, useState } from "react";
 // Internal components
 import { Button } from "@/components/ui/button";
 import {
@@ -10,11 +39,24 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { useToast } from "@/components/ui/use-toast";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
 
-export function WalletSelector() {
+
+export function WalletSelector(walletSortingOptions: WalletSortingOptions) {
+  // @ts-ignore
   const { account, connected, disconnect, wallets = [] } = useWallet();
-  const mizuWallet = wallets.find((w) => w.name === "Mizu Wallet");
+  // const mizuWallet = wallets.find((w) => w.name === "Mizu Wallet");
+  // console.log("mizuWallet", mizuWallet);
+  const closeDialog = useCallback(() => setIsDialogOpen(false), []);
+
   const { toast } = useToast();
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
 
   const copyAddress = useCallback(async () => {
     if (!account?.address) return;
@@ -33,9 +75,9 @@ export function WalletSelector() {
     }
   }, [account?.address, toast]);
 
-  if (!mizuWallet) {
-    return <>Mizu Wallet Not Found</>;
-  }
+  // if (!mizuWallet) {
+  //   return <>Mizu Wallet Not Found</>;
+  // }
 
   return connected ? (
     <DropdownMenu>
@@ -52,12 +94,203 @@ export function WalletSelector() {
       </DropdownMenuContent>
     </DropdownMenu>
   ) : (
-    <WalletItem wallet={mizuWallet} className="flex items-center justify-between px-4 py-3 gap-4 border rounded-md">
+    // <WalletItem wallet={mizuWallet} className="flex items-center justify-between px-4 py-3 gap-4 border rounded-md">
+    //   <div className="flex items-center gap-4">
+    //     <WalletItem.Icon className="h-6 w-6" />
+    //     <WalletItem.Name className="text-base font-normal" />
+    //   </div>
+    //   {isInstallRequired(mizuWallet) ? (
+    //     <Button size="sm" variant="ghost" asChild>
+    //       <WalletItem.InstallLink />
+    //     </Button>
+    //   ) : (
+    //     <WalletItem.ConnectButton asChild>
+    //       <Button size="sm">Connect</Button>
+    //     </WalletItem.ConnectButton>
+    //   )}
+    // </WalletItem>
+
+    <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+      <DialogTrigger asChild>
+        <Button>Connect a Wallet</Button>
+      </DialogTrigger>
+      <ConnectWalletDialog close={closeDialog} {...walletSortingOptions} />
+    </Dialog>
+  );
+}
+
+
+interface ConnectWalletDialogProps extends WalletSortingOptions {
+  close: () => void;
+}
+
+function ConnectWalletDialog({
+                               close,
+                               ...walletSortingOptions
+                             }: ConnectWalletDialogProps) {
+  const { wallets = [] } = useWallet();
+
+  const { aptosConnectWallets, availableWallets, installableWallets } =
+    groupAndSortWallets(wallets, walletSortingOptions);
+
+  const hasAptosConnectWallets = !!aptosConnectWallets.length;
+
+  return (
+    <DialogContent className="max-h-screen overflow-auto">
+      <AboutAptosConnect renderEducationScreen={renderEducationScreen}>
+        <DialogHeader>
+          <DialogTitle className="flex flex-col text-center leading-snug">
+            {hasAptosConnectWallets ? (
+              <>
+                <span>Log in or sign up</span>
+                <span>with Social + Aptos Connect</span>
+              </>
+            ) : (
+              "Connect Wallet"
+            )}
+          </DialogTitle>
+        </DialogHeader>
+
+        {hasAptosConnectWallets && (
+          <div className="flex flex-col gap-2 pt-3">
+            {aptosConnectWallets.map((wallet) => (
+              <AptosConnectWalletRow
+                key={wallet.name}
+                wallet={wallet}
+                onConnect={close}
+              />
+            ))}
+            <p className="flex gap-1 justify-center items-center text-muted-foreground text-sm">
+              Learn more about{" "}
+              <AboutAptosConnect.Trigger className="flex gap-1 py-3 items-center text-foreground">
+                Aptos Connect <ArrowRight size={16} />
+              </AboutAptosConnect.Trigger>
+            </p>
+            <AptosPrivacyPolicy className="flex flex-col items-center py-1">
+              <p className="text-xs leading-5">
+                <AptosPrivacyPolicy.Disclaimer />{" "}
+                <AptosPrivacyPolicy.Link className="text-muted-foreground underline underline-offset-4" />
+                <span className="text-muted-foreground">.</span>
+              </p>
+              <AptosPrivacyPolicy.PoweredBy className="flex gap-1.5 items-center text-xs leading-5 text-muted-foreground" />
+            </AptosPrivacyPolicy>
+            <div className="flex items-center gap-3 pt-4 text-muted-foreground">
+              <div className="h-px w-full bg-secondary" />
+              Or
+              <div className="h-px w-full bg-secondary" />
+            </div>
+          </div>
+        )}
+
+        <div className="flex flex-col gap-3 pt-3">
+          {availableWallets.map((wallet) => (
+            <WalletRow key={wallet.name} wallet={wallet} onConnect={close} />
+          ))}
+          {!!installableWallets.length && (
+            <Collapsible className="flex flex-col gap-3">
+              <CollapsibleTrigger asChild>
+                <Button size="sm" variant="ghost" className="gap-2">
+                  More wallets <ChevronDown />
+                </Button>
+              </CollapsibleTrigger>
+              <CollapsibleContent className="flex flex-col gap-3">
+                {installableWallets.map((wallet) => (
+                  <WalletRow
+                    key={wallet.name}
+                    wallet={wallet}
+                    onConnect={close}
+                  />
+                ))}
+              </CollapsibleContent>
+            </Collapsible>
+          )}
+        </div>
+      </AboutAptosConnect>
+    </DialogContent>
+  );
+}
+
+function renderEducationScreen(screen: AboutAptosConnectEducationScreen) {
+  return (
+    <>
+      <DialogHeader className="grid grid-cols-[1fr_4fr_1fr] items-center space-y-0">
+        <Button variant="ghost" size="icon" onClick={screen.cancel}>
+          <ArrowLeft />
+        </Button>
+        <DialogTitle className="leading-snug text-base text-center">
+          About Aptos Connect
+        </DialogTitle>
+      </DialogHeader>
+
+      <div className="flex h-[162px] pb-3 items-end justify-center">
+        <screen.Graphic />
+      </div>
+      <div className="flex flex-col gap-2 text-center pb-4">
+        <screen.Title className="text-xl" />
+        <screen.Description className="text-sm text-muted-foreground [&>a]:underline [&>a]:underline-offset-4 [&>a]:text-foreground" />
+      </div>
+
+      <div className="grid grid-cols-3 items-center">
+        <Button
+          size="sm"
+          variant="ghost"
+          onClick={screen.back}
+          className="justify-self-start"
+        >
+          Back
+        </Button>
+        <div className="flex items-center gap-2 place-self-center">
+          {screen.screenIndicators.map((ScreenIndicator, i) => (
+            <ScreenIndicator key={i} className="py-4">
+              <div className="h-0.5 w-6 transition-colors bg-muted [[data-active]>&]:bg-foreground" />
+            </ScreenIndicator>
+          ))}
+        </div>
+        <Button
+          size="sm"
+          variant="ghost"
+          onClick={screen.next}
+          className="gap-2 justify-self-end"
+        >
+          {screen.screenIndex === screen.totalScreens - 1 ? "Finish" : "Next"}
+          <ArrowRight size={16} />
+        </Button>
+      </div>
+    </>
+  );
+}
+
+function AptosConnectWalletRow({ wallet, onConnect }: WalletRowProps) {
+  return (
+    <WalletItem wallet={wallet} onConnect={onConnect}>
+      <WalletItem.ConnectButton asChild>
+        <Button size="lg" variant="outline" className="w-full gap-4">
+          <WalletItem.Icon className="h-5 w-5" />
+          <WalletItem.Name className="text-base font-normal" />
+        </Button>
+      </WalletItem.ConnectButton>
+    </WalletItem>
+  );
+}
+
+
+interface WalletRowProps {
+  wallet: AnyAptosWallet;
+  onConnect?: () => void;
+}
+
+function WalletRow({ wallet, onConnect }: WalletRowProps) {
+  return (
+    <WalletItem
+      wallet={wallet}
+      onConnect={onConnect}
+      className="flex items-center justify-between px-4 py-3 gap-4 border rounded-md"
+    >
       <div className="flex items-center gap-4">
         <WalletItem.Icon className="h-6 w-6" />
         <WalletItem.Name className="text-base font-normal" />
       </div>
-      {isInstallRequired(mizuWallet) ? (
+      {isInstallRequired(wallet) ? (
         <Button size="sm" variant="ghost" asChild>
           <WalletItem.InstallLink />
         </Button>
